529bc97964e7bb8403ca34743df04d50a663739e
Mohit  Yadav <79302660+Mohityadav-code@users.noreply.github.com>
2024-10-23 16:24:33 +0530

    ui updated style updated

--- client/src/components/Preview.js
+++ client/src/components/Preview.js
@@ -1,34 +1,48 @@
-// client/src/components/Preview.js
 import React, { useEffect, useRef } from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github.css'; // Ensure consistency with backend
+
+// client/src/components/Preview.js
 
 const Preview = ({ htmlContent }) => {
     const previewRef = useRef(null);
 
     useEffect(() => {
-      if (previewRef.current) {
-        // Highlight code blocks
-        hljs.highlightAll();
-      }
+        if (previewRef.current) {
+            // Highlight code blocks
+            hljs.highlightAll();
+        }
     }, [htmlContent]);
-  
-  return (
-    <Box
-      sx={{
-        padding: 2,
-        height: '100%',
-        overflow: 'auto',
-        backgroundColor: 'background.default',
-        color: 'text.primary',
-      }}
-       dangerouslySetInnerHTML={{ __html: htmlContent }}
-    >
-        
- 
-    </Box>
-  );
+
+    return (
+        <Box
+            sx={{
+             
+                height: '100%',
+                overflow: 'auto',
+                backgroundColor: 'background.default',
+                color: 'text.primary',
+            }}
+        >
+            <Typography variant="h6" sx={{
+                   paddingLeft:'10px',
+                   paddingTop:'9px',
+                   fontWeight: 'bold',
+            }} fontSize={16} gutterBottom>
+                Preview
+            </Typography>
+            <Box
+            sx={{
+                paddingLeft:'10px',
+                borderTop: '1px solid #ccc',
+            }}
+
+                ref={previewRef}
+                dangerouslySetInnerHTML={{ __html: htmlContent }}
+            />
+        </Box>
+    );
 };
 
 export default Preview;
